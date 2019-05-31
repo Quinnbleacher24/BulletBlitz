6c89e9da04e3be1f0ebb31c0a9564735731eae8b
jkmartin5500 <jkmartin5500@gmail.com>
2019-05-31 17:43:45 -0400

    Added Wall Coliders

--- Assets/EnemyControl.cs
+++ Assets/EnemyControl.cs
@@ -9,15 +9,16 @@ public class EnemyControl : MonoBehaviour
     public float vel_x;
     public float vel_y;
     public float speed;
+    public bool dead;
 
     // Start is called before the first frame update
     void Start()
     {
         pos_x = 8;
         pos_y = 0;
-        vel_x = 0;
-        vel_y = 0;
         speed = 0.05F;
+        setDirection();
+        dead = false;
     }
 
     // Update is called once per frame
@@ -25,11 +26,15 @@ public class EnemyControl : MonoBehaviour
     {
         transform.position = new Vector2(pos_x, pos_y);
         Move();
+        CheckStatus();
+        if (dead)
+        {
+            Start();
+        }
     }
 
     void Move()
     {
-        setDirection();
         pos_x += vel_x * speed;
         pos_y += vel_y * speed;
     }
@@ -40,5 +45,10 @@ public class EnemyControl : MonoBehaviour
         float distance = Mathf.Sqrt(Mathf.Pow(player_pos.x - pos_x, 2) + Mathf.Pow(player_pos.y - pos_y, 2));
         vel_x = (player_pos.x - pos_x) / distance;
         vel_y = (player_pos.y - pos_y) / distance;
+    }
+
+    void CheckStatus()
+    {
+        
     }
 }
---
+++ Assets/GameSetup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public Camera mainCam;
+    public BoxCollider2D Top_Wall;
+    public BoxCollider2D bottom;
+    public BoxCollider2D left;
+    public BoxCollider2D right;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 ScreenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+
+        Top_Wall.size = new Vector2(ScreenSize.x * 2, 1f);
+        Top_Wall.offset = new Vector2(0f, ScreenSize.y + 0.5f);
+
+        bottom.size = new Vector2(ScreenSize.x * 2, 1f);
+        bottom.offset = new Vector2(0f, -0.5f - ScreenSize.y);
+
+        left.size = new Vector2(1f, ScreenSize.y * 2);
+        left.offset = new Vector2(-0.5f - ScreenSize.x, 0f);
+
+        right.size = new Vector2(1f, ScreenSize.y * 2);
+        right.offset = new Vector2(ScreenSize.x + 0.5f, 0f);
+    }
+}
