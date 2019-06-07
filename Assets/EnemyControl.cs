e89e2eada9b820108d5e2f57090495c835bc44ad
jkmartin5500 <jkmartin5500@gmail.com>
2019-06-07 19:57:13 -0400

    Gave Enemy a respawn mechanic

--- Assets/EnemyControl.cs
+++ Assets/EnemyControl.cs
@@ -6,28 +6,33 @@ public class EnemyControl : MonoBehaviour
 {
     public Vector2 pos;
     public Vector2 vel;
-    public float speed = 0.05F;
+    public float speed;
     public bool dead;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        pos = new Vector2(8, -1);
+        pos = new Vector2(10, Random.Range(-6F, 6F));
         vel = setDirection();
         dead = false;
+        speed = 7;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        checkStatus();
         Move();
         transform.position = pos;
     }
 
     void Move()
     {
-        pos += vel * speed;
+        Vector2 movement = vel;
+
+        rb.velocity = movement * speed;
     }
 
     Vector2 setDirection()
@@ -37,8 +42,19 @@ public class EnemyControl : MonoBehaviour
         return distance / distance.magnitude;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (pos.x < 0)
+        {
+            dead = true;
+        }
+    }
+
     public void checkStatus()
     {
-
+        if(dead)
+        {
+            Start();
+        }
     }
 }
